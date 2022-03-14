7813939df940f3ae9add2ce76b1b1568c0ad057d
NoahAncolie <noahancolie@gmail.com>
2022-03-14 14:43:43 +0100

    ajout de stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/destroy'
-  get 'orders/create'
-  get 'orders/show'
-  get 'orders/update'
-  get 'orders/new'
-  get 'carts/index'
-  get 'carts/destroy'
-  get 'carts/create'
-  get 'carts/new'
-  get 'carts/show'
-  get 'carts/update'
-  get 'items/index'
-  get 'items/show'
-  get 'items/create'
-  get 'items/destroy'
-  get 'items/update'
-  get 'items/new'
   devise_for :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
