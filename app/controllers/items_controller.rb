b655489dfbba8713978d9282113d99e258cccc27
NoahAncolie <noahancolie@gmail.com>
2022-03-14 15:20:41 +0100

    controller item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,30 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def create
+    @item = Item.new(permit_params)
   end
 
   def destroy
+    Item.find(params[:id]).destroy
   end
 
   def update
+    Item.find(params[:id])
   end
 
   def new
+  end
+
+  private
+
+  def permit_params
+  
   end
 end
