ba2ba2d1fd8f23ce273fdea7490336cbd588ffea
NoahAncolie <noahancolie@gmail.com>
2022-03-14 20:10:30 +0100

    commits

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'items#index'
+  root to: 'landings#index'
   resources :items, path: 'chat' do
     resources :comments
   end
