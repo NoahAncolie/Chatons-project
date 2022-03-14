85ab7a549e305cfc93e4638262fc781902aff6ec
NoahAncolie <noahancolie@gmail.com>
2022-03-14 17:04:19 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+images_link = [ mettre les liens des images ici ]
+
+5.times do |i|
+    item = Item.new
+    item.picture.attach(images_link[i])
+    item.name = "Photo de chat"
+    item.price = "£100"
+    item.save
+end
