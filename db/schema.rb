5c8aefa77e2c57a6161d676dbcc6f428df0fab25
loicpaolucci <loic6701@laposte.net>
2022-03-15 15:07:22 +0100

    chat

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 2022_03_14_183613) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.integer "quantity"
+    t.boolean "in_cart"
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_carts_on_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id"
@@ -57,11 +68,11 @@ ActiveRecord::Schema.define(version: 2022_03_14_183613) do
   create_table "orders", force: :cascade do |t|
     t.boolean "achieved"
     t.bigint "item_id"
-    t.bigint "user_id"
+    t.bigint "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
