c69601bb2c2facecbd48509237354420dc95379e
NoahAncolie <noahancolie@gmail.com>
2022-03-15 15:17:55 +0100

    rajouter route order in comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landings#index'
   resources :items, path: 'chat' do
+    resources :orders
     resources :comments
   end
   resources :carts, only: [:create, :show, :update, :destroy], path: 'panier'
   resources :users, only: [:show, :update, :edit], path: 'profil'
   resources :charges, only: [:new, :create], path: 'paiement'
-  resources :orders
 end
