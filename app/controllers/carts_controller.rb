1d71c054d6eda072f0c870dde8e0f9f14a42ffe0
NoahAncolie <noahancolie@gmail.com>
2022-03-15 23:08:46 +0100

    commits

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,7 +5,13 @@ class CartsController < ApplicationController
   end
 
   def create
-    @cart-item = Cart.new()
+    @cart_item = CartItem.new(permit_params)
+    if @cart_item.save
+      redirect_to '/'
+    else 
+      flash.now[:error] = puts @cart_item.errors.messages
+      render 'chat'
+    end
   end
 
   def new
@@ -23,5 +29,15 @@ class CartsController < ApplicationController
   end
 
   private
+
+  def find_price
+    return Item.find(params[:item_id].to_i).price * params[:quantity].to_i
+  end
+
+  def permit_params
+    params[:price] = find_price
+    params[:user_id] = current_user.id
+    params.permit(:item_id, :user_id, :quantity, :price)
+  end
 
 end
