222b7da114a5a5aac6077f51f9a3e8207677312a
NoahAncolie <noahancolie@gmail.com>
2022-03-16 19:52:30 +0100

    working on nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
-2.times do |i|
+13.times do |i|
     item = Item.new
-    item.picture.attach(io: File.open(File.join(Rails.root,"app/assets/images/chatCarousel#{i+1}.jpg")), filename: 'chat.jpg')
-=======
-8.times do |i|
-    item = Item.new
-    item.picture.attach(io: File.open(File.join(Rails.root,"app/assets/images/#{i}.jpg")), filename: 'chat.jpg')
->>>>>>> cats-shop
+    item.picture.attach(io: File.open(File.join(Rails.root,"app/assets/images/cat#{i+ 1}.jpg")), filename: 'chat.jpg')
     item.name = "Photo de chat"
     item.price = 100
     item.save
