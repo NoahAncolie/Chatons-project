cd95438f751b4f1f544b120d779508f175c394c9
NoahAncolie <noahancolie@gmail.com>
2022-03-16 16:05:39 +0100

    some changes in payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
     include CartsHelper
-<<<<<<< HEAD
-=======
     include ChargesHelper
->>>>>>> cats-shop
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,14 +8,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(item_id: params[:item_id], customer_id: current_user.id, achieved: false)
     if @order.save
-<<<<<<< HEAD
-      respond_to do |format|
-        format.html { redirect_to '/' }
-        format.js { }
-      end
-=======
       redirect_to new_charge_path
->>>>>>> cats-shop
     else
       flash.now[:error] = "ERRORRR"
     end
