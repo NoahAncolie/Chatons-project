b5311eefb5aa5fc3f34361ea0c5ef50bf82bed6a
loicpaolucci <loic6701@laposte.net>
2022-03-17 15:04:27 +0100

    cat

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,15 +1,18 @@
 class AdminMailer < ApplicationMailer
-    default from: 'catstore@yopmail.com'
+
+    default from: 'catshop@yopmail.com'
+
     def order_email(order)
-        @order = order
-        @cart = @order.user.cart
-        @user = @order.user
-    
-        @admin = User.find_by(is_admin: true)
-    
-        @url  = 'https://cat-store-thp.herokuapp.com/'
-    
-        mail(to: @admin.email, subject: 'Un nouvel achat a été effectué')
-      end
-    
+      @order = order
+      @cart = @order.user.cart
+      @user = @order.user
+  
+      @admin = User.find_by(is_admin: true)
+  
+      @url  = 'https://cats-shop-thp.herokuapp.com/'
+  
+      mail(to: @admin.email, subject: 'Un nouvel achat a été effectué')
+    end
+  
+
 end
